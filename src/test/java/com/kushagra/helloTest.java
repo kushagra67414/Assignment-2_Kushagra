b80eeaef763c806f3969dc33c95a63ed2dba8eaa
kushagra67414 <500067414@stu.upes.ac.in>
2020-11-24 01:46:24 +0530

    hi

---
+++ src/test/java/com/kushagra/helloTest.java
@@ -0,0 +1,15 @@
+package com.kushagra;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class helloTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello Maven", new String("Hello Maven"));	}
+
+}
